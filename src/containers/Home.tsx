2f9a319fb9bfccd9033d678ed8c9fce807a6f230
Hannah Akhtar <hannahaakhtar@gmail.com>
2022-01-06 16:48:40 +0000

    axios request updated to include 5 day weather forecast

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -7,19 +7,23 @@ const Home = () => {
 
     const [typed, setTyped] = useState('')
     const [cityData, setCityData] = useState<AxiosResponse>()
-    
+
     interface AxiosResponse {
         location: object,
-        current: object
+        current: object,
+        forecast?: object
     }
 
-     const handleSubmit = async (e: React.SyntheticEvent) => {
+    const handleSubmit = async (e: React.SyntheticEvent) => {
         e.preventDefault()
         const searchCapitalised = typed.charAt(0).toUpperCase() + typed.slice(1)
-        const response = await axios.get<AxiosResponse>(`https:api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${searchCapitalised}&aqi=no`)
-        setCityData(response.data)
+        const response = await axios.get<AxiosResponse>(`https:api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${searchCapitalised}&days=5&aqi=no&alerts=no`)
+        if (Object.keys(response).length === 0) {
+            // logic to display 'no results' text 
+        } else {
+            setCityData(response.data)
+        }
     }
-
 
     return <>
         <div>
